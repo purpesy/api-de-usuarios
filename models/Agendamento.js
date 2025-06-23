3bb434479754450edac034724340eef30f169431
purpesy <lucasgabrieldossantos753@gmail.com>
2025-06-23 12:58:55 -0300

    Fiz o model para criar, e arrumei o model para encontrar os agendamentos

--- models/Agendamento.js
+++ models/Agendamento.js
@@ -1,30 +1,60 @@
 var knex = require("../database/connection.js");
 class Agendamento {
-    async findAll() {
-        // Listar todos os agendamentos
-        try {
-            var result = await knex.select(["*"]).table("tbl_agendamentos").orderBy("data_agendamento", "desc");
-            return result; // Retorna todos os agendamentos encontrados
-        } catch (error) {
-            console.log(error);
-            return []; // Retorna um array vazio em caso de erro
-        }
+  async findAll() {
+    // Listar todos os agendamentos
+    try {
+      var result = knex("tbl_agendamento as a")
+        .leftJoin("tbl_users as u", "a.id_user", "u.id_user")
+        .leftJoin("tbl_servicos as s", "a.id_servico", "s.id_servico")
+        .select(
+          "a.id_agendamento",
+          "a.obs_agendamento",
+          "a.data_agendamento",
+          "a.status_agendamento",
+          "u.nome_user",
+          "u.email_user",
+          "s.nome_servico"
+        );
+      return result;
+    } catch (error) {
+      console.log(error);
+      return [];
     }
+  }
 
-    async findById(id) {
-        // Buscar agendamento por ID
-        try {
-            var result = await knex.select().table("tbl_agendamentos").where({ id_agendamento: id });
-            if (result.length > 0) {
-                return result[0]; // Retorna o primeiro agendamento encontrado
-            } else {
-                return null; // Retorna null se nenhum agendamento for encontrado
-            }
-        } catch (error) {
-            console.log(error);
-            return null; // Retorna null em caso de erro
-        }
+  async findById(id) {
+    // Buscar agendamento por ID
+    try {
+      var result =  await knex("tbl_agendamento as a")
+        .leftJoin("tbl_users as u", "a.id_user", "u.id_user")
+        .leftJoin("tbl_servicos as s", "a.id_servico", "s.id_servico")
+        .select(
+          "a.id_agendamento",
+          "a.obs_agendamento",
+          "a.data_agendamento",
+          "a.status_agendamento",
+          "u.nome_user",
+          "u.email_user",
+          "s.nome_servico"
+        ).where({ id_agendamento: id });
+      if (result.length > 0) {
+        return result[0]; 
+      } else {
+        return null;
+      }
+    } catch (error) {
+      console.log(error);
+      return null; // Retorna null em caso de erro
     }
+  }
+
+  async new(id_usuario, id_servico, data_agendamento) {
+    try {
+      await knex.insert({ id_usuario, id_servico, data_agendamento }).table("tbl_agendamento");
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
 
 module.exports = new Agendamento();
